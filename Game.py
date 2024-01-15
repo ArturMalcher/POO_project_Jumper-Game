846ed5d8e1d6e8b0e48c5b6a4022da55c51e1af1
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-01-15 19:07:53 -0300

    Change_array e Diagrama UML

--- Game.py
+++ Game.py
@@ -1,32 +1,52 @@
 import modules
 import classes
+import threading
+
 
 # Criando uma Tela
-screen1 = classes.Screen(140, 15, " ")
-
+screen1 = classes.Screen("screen1", 140, 10, " ")
 
 # Criando Sprites
-ground = classes.Sprite(screen1, 140, 1, (1, 1), "_")
-player = classes.Sprite(screen1, 2, 2, (2, 2), " | ")
-enemy1 = classes.Sprite(screen1, 1, 2, (138, 2), "##")
-enemy2 = classes.Sprite(screen1, 1, 2, (100, 2), " ! ")
-enemy3 = classes.Sprite(screen1, 1, 2, (54, 2), "#")
+ground = classes.Sprite("ground", 140, 1, (1, 1), "_")
+player = classes.Sprite("player", 3, 2, (2, 2), "$")
+
+player_corpo = {1:{1:"%", 2:"%", 3:">"},
+                2:{1:"!", 2:"!", 3:" "}}
+
+# Mudando o corpo de player para array
+player.change_array(player_corpo)
+
+# Criando inimigos
+enemy1 = classes.Sprite("enemy1", 1, 2, (138, 2), "#")
+enemy2 = classes.Sprite("enemy2", 2, 1, (120, 5), "=")
+enemy3 = classes.Sprite("enemy3", 1, 2, (138, 2), "#")
+enemy4 = classes.Sprite("enemy4", 1, 2, (138, 2), "#")
+enemy5 = classes.Sprite("enemy5", 1, 2, (138, 2), "#")
+enemy6 = classes.Sprite("enemy6", 1, 2, (138, 2), "#")
 
 # Adicionando Sprites na tela
 screen1.add_sprite(ground) 
 screen1.add_sprite(player)
-screen1.add_sprite(enemy1)
-screen1.add_sprite(enemy2)
-screen1.add_sprite(enemy3)
-
+timer = 0.02
 score = 0
 frame = 0
+thread_lock = threading.Lock()
+
+
+     
 while modules.running:
-    
-    modules.wait(modules.run, screen1, player, enemy1, enemy2, enemy3)
+        
+        
     # Esperar resposta
+    with thread_lock:
+        thread_input = threading.Thread(target=modules.run, args=(screen1, player))
+        thread_input.start()
+        thread_input.join(timeout=timer)
+        if not thread_input.is_alive():
+            modules.run(screen1, player)
 
     # Limpar display
+    #modules.run(screen1, player)
     modules.clear()
     score += 1
     
@@ -37,10 +57,8 @@ while modules.running:
     
 
 modules.clear()
-screen1 = classes.Screen(140, 15, "/")
-gameover = classes.Sprite(screen1, 1, 1, (60, 6), "  G A M E  O V E R  ")
-score_obj = classes.Sprite(screen1, 1, 1, (60, 5), f"  S C O R E: {score}  ")
+gameover = classes.Sprite("gameover", 1, 1, (60, 6), "  G A M E  O V E R  ")
+score_obj = classes.Sprite("score", 1, 1, (60, 5), f"  S C O R E: {score}  ")
 screen1.add_sprite(score_obj)
 screen1.add_sprite(gameover)
 screen1.display()
-
--- modules.py
+++ modules.py
@@ -1,83 +1,65 @@
 import os
-import threading
 import msvcrt
 import classes
 import random
 
-timer = 0.02
 running = True
-frame = 0
 jump = False
 falling = False
 cont = 0
-
+frame = 0
 
 # Limpar display
 def clear():
-        return os.system("cls" if os.name in ("nt", "dos") else "clear")
+    return os.system("cls" if os.name in ("nt", "dos") else "clear")
 
-# Frame a frame
-
+# Controles
 def controls(sprite):
     global running
     global jump
-    # Controles
+
+    # Receber resposta do teclado
     resp = msvcrt.getch().decode('utf-8')
+
+    # Movimentação AWD
     if resp:
         if resp[0] == "a":
             sprite.move('left', 2)
         
         elif resp[0] == "d":
             sprite.move('right', 2)
-        
-        elif resp[0] == "q":
-            running = False
     
         elif resp[0] == "w":
             if jump == False and falling == False:
                 jump = True
-    
-        elif resp[0] == "s":
-            sprite.move('down', 1)
 
+        # Sair
+        elif resp[0] == "q":
+            running = False
+    
 # AQUI É ONDE RODA TUDO
-def run(screen, sprite, enemy1, enemy2, enemy3):
+def run(screen, sprite):
     global frame
     global running
     global jump
     global falling
     global cont
     
-    if sprite.position in [enemy1.position, enemy2.position, enemy3.position]:
-        running = False
+    for sprite_obj in classes.Sprite.dict_id.values():
         
-    if enemy1.id in screen.dic_sprites:
-        enemy1.move('left', 2)
-        if enemy1.position[0] < 1:
-            enemy1.position = (138, enemy1.position[1])
-            screen.del_sprite(enemy1.id)   
-    else:
-        if random.randint(1, 100) == 1:
-            screen.add_sprite(enemy1)
+        if "enemy" in sprite_obj.name:
+            if sprite_obj in screen.dic_sprites.copy().values():
+                sprite_obj.move('left', 2)
+                if sprite_obj.position[0] < 1:
+                    sprite_obj.position = (138, sprite_obj.position[1])
+                    screen.del_sprite(sprite_obj.id)  
+                
+                if sprite.position == sprite_obj.position:
+                    running = False
+            else:
+                if random.randint(1, 100) == 1:
+                    screen.add_sprite(sprite_obj)
     
-    if enemy2.id in screen.dic_sprites:
-        enemy2.move('left', 2)
-        if enemy2.position[0] < 2:
-            enemy2.position = (138, enemy2.position[1])
-            screen.del_sprite(enemy2.id)   
-    else:
-        if random.randint(1, 100) == 100:
-            screen.add_sprite(enemy2)
-    
-    if enemy3.id in screen.dic_sprites:
-        enemy3.move('left', 2)
-        if enemy3.position[0] < 2:
-            enemy3.position = (138, enemy3.position[1])
-            screen.del_sprite(enemy3.id)   
-    else:
-        if random.randint(1, 100) == 20:
-            screen.add_sprite(enemy3)
-
     # Em caso de estar pulando
     if jump:
         sprite.move('up', 2)
@@ -85,23 +67,12 @@ def run(screen, sprite, enemy1, enemy2, enemy3):
             jump = False
             falling = True
     
-    # Caindo
-    elif falling:
+    # Em caso de estar caindo
+    elif falling:   
         sprite.move('down', 2)
         if sprite.position[1] < 3:
             falling = False
-    
+
     controls(sprite)
-
-    
-
-# Esperar resposta
-def wait(funct, screen, sprite, enemy1, enemy2, enemy3):
-        global thread_ativa
-        thread_input = threading.Thread(target=funct, args=(screen, sprite, enemy1, enemy2, enemy3))
-        thread_input.start()
-        thread_input.join(timeout=timer)
-
-        if not thread_input.is_alive():
-            funct(screen, sprite, enemy1, enemy2, enemy3)
+            
 

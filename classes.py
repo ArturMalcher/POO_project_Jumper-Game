b8f5ddc3a23dd382924400428dae0d66855757b3
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-01-15 19:07:01 -0300

    Change_array e Diagrama UML

--- classes.py
+++ classes.py
@@ -1,56 +1,82 @@
-from typing import Tuple, List, Dict
+from typing import Tuple, Dict
 
 # TUDO QUE APARECE NA TELA
 class Screen:
     
     # Construtor
-    def __init__(self, width: int, height: int, background: str):
+    def __init__(self, name: str, width: int, height: int, background: str):
+        self.name = name
         self.width = width
         self.height = height
         self.background = background
         self.dic_sprites: Dict[int, Sprite] = {}
-    
-    # Mostrar o objeto com base na matriz criada
-        
-    def display(self):
-        bg = self.background
+        array: Dict[int, Dict[int, str]] = {}
 
-        # Redesenhando os sprites adicionados ao display
-        array = {}
-
+        # Array de dicionário com numeração e caracteres
         for y in range(1, self.height+1):
             array[y] = {}
             for x in range(1, self.width+1):
                 array[y][x] = self.background
 
+        # Array de lista default 
+        array_default = []
+        for y in array.keys():
+            array_default.append([array[y][x] for x in array[y].keys()])
+
+        self.array = array
+        self.array_default = array_default
+
+    def change_array(self, array):
+
+        array_default = []
+        for y in array.keys():
+            array_default.append([array[y][x] for x in array[y].keys()])
+
+        self.array = array
+        self.array_default = array_default
+        
+    # Mostrar o objeto com base na matriz criada
+    def display(self):
+        array = list(map(list, self.array_default))
+        
+        # Desenhando os sprites adicionados ao display
         for sprite_id in self.dic_sprites:
             sprite = Sprite.dict_id[sprite_id]
-            width = sprite.width
-            height = sprite.height
+            sprite_w = sprite.width
+            sprite_h = sprite.height
             x = sprite.position[0]
             y = self.height - sprite.position[1]
+            #print(f'{sprite.name}: x:{x}, y:{y}, w:{sprite_w}, h:{sprite_h}')            
 
-            # Refazendo a matriz do objeto com base no sprite adicionado
+            # desenhando coluna por coluna, de linha em linha
+            for char_y, sprite_y in enumerate(range(y, y + sprite_h)):
+                for char_x, sprite_x in enumerate(range(x, x + sprite_w)):
+                    #print(f"{sprite.name}: {char_x}, {char_y}")
+                    array[sprite_y][sprite_x -1] = sprite.array[char_y +1][char_x +1]
 
-            for py in range(y, y + height):
-                for px in range(x, x + width):
-                    array[py][px] = sprite.material
-        
         # Imprimindo o Array atualizado
         for y in array:
             # Caso não tenha nada dentro de um px, então mostra o Background
-            for x in array[y]:
-                if array[y][x] == None:
-                    print(bg, end='')
-                else:
-                    print(array[y][x], end='')
+            for x in y:
+                    print(x, end='')
             print()
-            
 
-    
+         # Imprimindo o Array pelo dicionário
+        #for y in array:
+            # Caso não tenha nada dentro de um px, então mostra o Background
+
+        #    for x in array[y]:
+        #        if array[y][x] == None:
+        #            print(bg, end='')
+        #         else:
+        #            print(array[y][x], end='')
+        #    print()
+
     # Adicionar um sprite ao array do Screen
     def add_sprite(self, sprite):
         self.dic_sprites[sprite.id] = sprite
+        sprite.set_screen(self)
+        
 
     def del_sprite(self, id):
         del self.dic_sprites[id]
@@ -59,11 +85,9 @@ class Screen:
 class Sprite(Screen):
     dict_id: dict = {}
 
-    def __init__(self, screen: Screen, width: int, height: int, position: Tuple[int, int], material: str):
-        super().__init__(width, height, material)
-        self.material = material
-        self.screen = screen
-        self.body = material
+    def __init__(self, name:str, width: int, height: int, position: Tuple[int, int], background: str):
+        super().__init__(name, width, height, background)
+        self.background = background
         self.position = position
 
         # Definindo Id do sprite
@@ -72,6 +96,9 @@ class Sprite(Screen):
             id +=1
         self.id = id
         Sprite.dict_id[id] = self
+    
+    def set_screen(self, screen):
+        self.screen = screen
     
     # Método de movimentação de sprite
     def move(self, direction, steps):
@@ -92,3 +119,30 @@ class Sprite(Screen):
             
         if direction == "right":
             self.position = (pos[0] + steps, pos[1])
+
+
+# Teste
+# Criação de objetos
+screen1 = Screen("screen1", 140, 10, ".")
+ground = Sprite("ground", 140, 1, (1, 1), "_")
+player = Sprite("player", 3, 2, (2, 2), " | ")
+enemy1 = Sprite("enemy1", 1, 2, (138, 2), "##")
+enemy2 = Sprite("enemy2", 1, 2, (138, 2), "[]")
+enemy3 = Sprite("enemy3", 1, 2, (138, 2), "#")
+enemy4 = Sprite("enemy4", 1, 2, (138, 2), "#")
+enemy5 = Sprite("enemy5", 1, 2, (138, 2), "#")
+enemy6 = Sprite("enemy6", 1, 2, (138, 2), "#")
+
+# Adicionando objetos à screen1
+screen1.add_sprite(ground) 
+screen1.add_sprite(player) 
+
+# Criando um array para corpo
+array = {1:{1:"%", 2:"%", 3:">"},
+         2:{1:"!", 2:"!", 3:" "}}
+
+# Mudando o corpo de player para array
+player.change_array(array)
+
+screen1.display()
+
